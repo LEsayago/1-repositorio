13cb6a9f801ec6b368982083c8e3407644964ef6
LEsayago <lucas.e.sayago@gmail.com>
2023-08-08 18:04:21 -0300

    modificando la clase del main

--- PruebaRepositorio1/src/pruebarepositorio1/PruebaRepositorio1.java
+++ PruebaRepositorio1/src/pruebarepositorio1/PruebaRepositorio1.java
@@ -5,17 +5,14 @@
  */
 package pruebarepositorio1;
 
-/**
- *
- * @author Lucas E. Sayago
- */
+
 public class PruebaRepositorio1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        System.out.println("Solo vemos si esto Funciona (^_^)");
+        
     }
     
 }
