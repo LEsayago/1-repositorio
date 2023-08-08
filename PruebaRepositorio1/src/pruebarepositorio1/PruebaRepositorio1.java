49624d5a8a54e75d1181e21990558b733a3bb040
Gaabbii <gabil_racing@hotmail.com>
2023-08-08 19:53:36 -0300

    hola

--- PruebaRepositorio1/src/pruebarepositorio1/PruebaRepositorio1.java
+++ PruebaRepositorio1/src/pruebarepositorio1/PruebaRepositorio1.java
@@ -17,6 +17,7 @@ public class PruebaRepositorio1 {
         System.out.println("asdjaskdfjaskfjwek enzo olakease :v");
         System.out.println("algo coherente");
         System.out.println("Esto es el ejer.....");
+        System.out.println("a ver que onnda?");
 
     }
 
